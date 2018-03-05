cb9d5ae9c076d046439119a33311cf27dd03f117
David Gimmig <david.gimmig@gmail.com>
2018-03-05 14:30:09 +0100

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -35,7 +35,6 @@ module.exports = function(config) {
       environment: 'dev'
     },
     reporters: ['progress', 'kjhtml'],
-    port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: false,
